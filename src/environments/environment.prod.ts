118aafae615386fd7bc409cfd537643b8163f0e6
Diwakar Adhikari <diwakar.adhikari@bajajfinserv.in>
2022-03-15 17:32:19 +0530

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB_B_a1kgFjNGz3lfySMOHgrvrAsVTOEC4",
+    authDomain: "fit-track-474e4.firebaseapp.com",
+    projectId: "fit-track-474e4",
+    storageBucket: "fit-track-474e4.appspot.com",
+    messagingSenderId: "105823075835",
+    appId: "1:105823075835:web:bec5383a3f4027c9c9df5e",
+    measurementId: "G-0E6FFHWD9N"
+  }
 };
